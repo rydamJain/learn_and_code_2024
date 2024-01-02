db3089336a30e92e1489777724d93d53834db77a
rydamJain <79462230+rydamJain@users.noreply.github.com>
2024-01-02 08:57:28 +0530

    Create testcases.py

---
+++ testcases.py
@@ -0,0 +1,9 @@
+# Test cases
+test_kaprekar_routine_with_trailing_zero_number():
+  kaprekar_routine(8920)
+  
+test_kaprekar_routine_with_repeating_digits():
+  kaprekar_routine(1111)
+
+test_kaprekar_routine_with_leading_zero_number():
+  kaprekar_routine(0419)
